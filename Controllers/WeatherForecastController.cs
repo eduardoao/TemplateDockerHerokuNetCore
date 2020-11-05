286f55fcc14fcc9c7611cfb53efa8dd3da8e27cc
EduardoAlcantara <eoalcantara@gmail.com>
2020-11-05 18:13:58 -0300

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace SampleHerokuContainer.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing23", "B56acing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing23", "Hummmm cool", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
